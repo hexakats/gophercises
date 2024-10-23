e7e7fffeee78231ef023c3e53116af825a0f09b2
alae <172059040+mossb0x@users.noreply.github.com>
2024-10-23 11:32:06 +0100

    quiz: basic game loop is done
    points, point gain on correct answers, loss on incorrect answers and
    game over when you reach negative points.

--- quiz/main.go
+++ quiz/main.go
@@ -3,23 +3,70 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"log"
-	"math/rand"
+	"math/rand/v2"
 	"os"
 )
 
-func readCsvFile(filePath String) [][]string {
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
 
-	return quiz
+	csvReader := csv.NewReader(f)
+	questions, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+	return questions
+}
+
+func askRandomQuestion(questions [][]string) {
+	points := 0
+	question_number := rand.IntN(len(questions) - 1)
+	question := questions[question_number][0]
+	answer := questions[question_number][1]
+
+	loop := true
+
+	for loop {
+		fmt.Println("What's " + question + "?")
+		var response string
+		fmt.Scanln(&response)
+
+		if string(response) != string(answer) {
+			points--
+			fmt.Printf("This answer is incorrect, -1 point, You now have %d points!\n", points)
+			if points < 0 {
+				println("GAME OVER")
+				loop = false
+			}
+			question_number = rand.IntN(len(questions) - 1)
+			question = questions[question_number][0]
+			answer = questions[question_number][1]
+		} else {
+			points++
+			fmt.Printf("This answer is correct! +1 point, You now have %d points! \n", points)
+			question_number = rand.IntN(len(questions) - 1)
+			question = questions[question_number][0]
+			answer = questions[question_number][1]
+		}
+	}
+	return
 }
 
 func main() {
-	file := flag.String("csv", "problems.csv", "Reads a csv file with questions in the first column and answers in the second column.")
+	filePath := flag.String("csv", "problems.csv", "Reads a CSV file with questions in the first column and answers in the second column.")
+	quiz := readCsvFile(*filePath)
+
 	time := flag.Int("timer", 0, "Sets a timer of n seconds")
 	flag.Parse()
-
-	_ = file
 	_ = time
 
-	println("hello world")
+	askRandomQuestion(quiz)
+
+	println("Goodbye!")
 }
