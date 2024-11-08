d23f614df05b5959417f18ce1e0c9624913be68d
alae <172059040+mossb0x@users.noreply.github.com>
2024-11-08 23:42:06 +0100

    quiz: finished part 1 and 2

--- quiz/main.go
+++ quiz/main.go
@@ -5,11 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand/v2"
+	"math/rand"
 	"os"
+	"time"
 )
 
-func readCsvFile(filePath string) [][]string {
+func getQuestionsFromCsv(filePath string, shuffle bool) [][]string {
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal("Unable to read input file "+filePath, err)
@@ -21,52 +22,55 @@ func readCsvFile(filePath string) [][]string {
 	if err != nil {
 		log.Fatal("Unable to parse file as CSV for "+filePath, err)
 	}
+
+	if shuffle {
+		for i := range questions {
+			j := rand.Intn(i + 1)
+			questions[i], questions[j] = questions[j], questions[i]
+		}
+	}
 	return questions
 }
 
-func askRandomQuestion(questions [][]string) {
+func askQuestion(questions [][]string) {
 	points := 0
-	question_number := rand.IntN(len(questions) - 1)
-	question := questions[question_number][0]
-	answer := questions[question_number][1]
+	question_number := 0
 
-	loop := true
-
-	for loop {
-		fmt.Println("What's " + question + "?")
+	for true {
+		fmt.Println("What's " + questions[question_number][0] + "?")
 		var response string
 		fmt.Scanln(&response)
 
-		if string(response) != string(answer) {
-			points--
-			fmt.Printf("This answer is incorrect, -1 point, You now have %d points!\n", points)
-			if points < 0 {
-				println("GAME OVER")
-				loop = false
-			}
-			question_number = rand.IntN(len(questions) - 1)
-			question = questions[question_number][0]
-			answer = questions[question_number][1]
+		if string(response) != string(questions[question_number][1]) {
+			fmt.Println("This answer is incorrect.")
 		} else {
 			points++
 			fmt.Printf("This answer is correct! +1 point, You now have %d points! \n", points)
-			question_number = rand.IntN(len(questions) - 1)
-			question = questions[question_number][0]
-			answer = questions[question_number][1]
+		}
+		question_number++
+		if question_number == len(questions) {
+			break
 		}
 	}
+	fmt.Printf("You scored %1d out of %2d in the quiz.\n", points, len(questions))
 	return
+}
+
+func gameTimer(duration int) {
+	timer := time.NewTimer(time.Duration(duration) * time.Second)
+	<-timer.C
+	fmt.Println("Timeout, You fail.")
+	os.Exit(1)
 }
 
 func main() {
 	filePath := flag.String("csv", "problems.csv", "Reads a CSV file with questions in the first column and answers in the second column.")
-	quiz := readCsvFile(*filePath)
+	limit := flag.Int("limit", 30, "Sets a timer of n seconds. default is -limit=30")
+	shuffle := flag.Bool("shuffle", false, "Shuffles the question order when used.")
 
-	time := flag.Int("timer", 0, "Sets a timer of n seconds")
 	flag.Parse()
-	_ = time
+	questions := getQuestionsFromCsv(*filePath, *shuffle)
 
-	askRandomQuestion(quiz)
-
-	println("Goodbye!")
+	go gameTimer(*limit)
+	askQuestion(questions)
 }
